86a8e19a80686bfa06ae8749f8570a8a7ea52721
aysenuron <onaranays@gmail.com>
2025-01-27 10:00:25 +0100

    Mobile cta buttons not showing when deployed but show on local. Trying to fix it.

--- src/script.js
+++ src/script.js
@@ -1,16 +1,16 @@
 const burgerIcon = document.querySelector(".burger-icon");
 const closeIcon = document.querySelector(".close-icon");
 
-const mobileMenuOpen = document.querySelector(".mobile-menu-open");
+const nav = document.querySelector("nav");
 
 burgerIcon.addEventListener("click", () => {
-    mobileMenuOpen.classList.remove("hidden");
+    nav.classList.remove("hidden");
     burgerIcon.classList.add("hidden");
     closeIcon.classList.remove("hidden");
 });
 
 closeIcon.addEventListener("click", () => {
-    mobileMenuOpen.classList.add("hidden");
+    nav.classList.add("hidden");
     burgerIcon.classList.remove("hidden");
     closeIcon.classList.add("hidden");
 })
