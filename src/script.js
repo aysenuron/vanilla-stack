1c27513a2965be81a9eab35ff809d4fdf3f282de
aysenuron <onaranays@gmail.com>
2025-01-27 10:22:20 +0100

    Fix cta buttons.

--- src/script.js
+++ src/script.js
@@ -1,16 +1,19 @@
 const burgerIcon = document.querySelector(".burger-icon");
 const closeIcon = document.querySelector(".close-icon");
 
-const nav = document.querySelector("nav");
+const mobileMenuOpen = document.querySelector(".mobile-menu-open");
+const ctaButtonsMobile = document.querySelector("#cta-buttons-mobile");
 
 burgerIcon.addEventListener("click", () => {
-    nav.classList.remove("hidden");
+    mobileMenuOpen.classList.remove("hidden");
+    ctaButtonsMobile.classList.remove("hidden");
     burgerIcon.classList.add("hidden");
     closeIcon.classList.remove("hidden");
 });
 
 closeIcon.addEventListener("click", () => {
-    nav.classList.add("hidden");
+    mobileMenuOpen.classList.add("hidden");
+    ctaButtonsMobile.classList.add("hidden");
     burgerIcon.classList.remove("hidden");
     closeIcon.classList.add("hidden");
 })
